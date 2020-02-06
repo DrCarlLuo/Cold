4d193323d2d2f4d75312bbd537f197373c5e8c33
misdeer <719851601@qq.com>
2020-02-06 21:36:43 +0800

    fix bugs

--- Development/Cold/Assets/Scripts/BehaviorController/ClawController.cs
+++ Development/Cold/Assets/Scripts/BehaviorController/ClawController.cs
@@ -4,7 +4,6 @@ namespace Cold
 {
   using TeamMask = Const.TeamMask;
   public class ClawController : MonoBehaviour{
-    public bool IsBusy{get; private set;} = false;
     public void Attack(TeamMask team, float damage){
       Collider2D[] ret = Physics2D.OverlapCircleAll(
         transform.position, 0.15f, LayerMask.GetMask("Pawn")
@@ -18,14 +17,13 @@ namespace Cold
       }
     }
     public void PickAndDrop(){
-      if(!IsBusy){
+      if(transform.childCount==0){
         Collider2D[] ret = Physics2D.OverlapCircleAll(
           transform.position, 0.15f, LayerMask.GetMask("Pawn")
         );
         foreach(var col in ret){
           var portable = col.transform.GetComponent<IPortable>();
           if(portable != null){
-            IsBusy = true;
             col.transform.SetParent(transform);
             col.transform.localPosition = Vector3.zero;
             portable.IsHold = true;
@@ -33,14 +31,11 @@ namespace Cold
           }
         }
       }
-      else if(transform.childCount>0){
+      else {
         var trans = transform.GetChild(0);
         var portable = trans.GetComponent<IPortable>();
         trans.SetParent(GameCore.I.SceneRoot, true);
         portable.IsHold = false;
-        if(transform.childCount <= 0){
-          IsBusy = false;
-        }
       }
     }
   }
--- Development/Cold/Assets/Scripts/GameCore.cs
+++ Development/Cold/Assets/Scripts/GameCore.cs
@@ -7,7 +7,9 @@ namespace Cold
   {
     #region inspector
     [SerializeField] Transform heroTrans = null;
+    [SerializeField] Transform woodTrans = null;
     [SerializeField] Transform sceneRoot = null;
+    [SerializeField] int initWoodCount;
     #endregion
     public static GameCore I{get; private set;}
     [HideInInspector] public HeroState hero = null;
@@ -20,6 +22,13 @@ namespace Cold
     }
     void Start(){
       
+      for(int i=0; i<initWoodCount; i++){
+        float p = 1f + 3f*Random.Range(0, 1f);
+        float t = 360f*Random.Range(0,1f);
+        var pos = new Vector3(p*Mathf.Cos(t), p*Mathf.Sin(t), 0);
+        Transform wood = Instantiate(woodTrans, pos, Quaternion.identity);
+        wood.SetParent(sceneRoot);
+      }
     }
   }
 }
