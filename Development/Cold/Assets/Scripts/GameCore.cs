4432236e8b0bbd2ca69bd1b132bfc8ac9f08e4f5
misdeer <719851601@qq.com>
2020-02-06 23:04:21 +0800

    version 0.1

--- Development/Cold/Assets/Scripts/GameCore.cs
+++ Development/Cold/Assets/Scripts/GameCore.cs
@@ -6,29 +6,56 @@ namespace Cold
   public class GameCore : MonoBehaviour
   {
     #region inspector
-    [SerializeField] Transform heroTrans = null;
-    [SerializeField] Transform woodTrans = null;
+    [SerializeField] Transform pfHero = null;
+    [SerializeField] Transform pfWood = null;
+    [SerializeField] Transform pfEnemy = null;
     [SerializeField] Transform sceneRoot = null;
-    [SerializeField] int initWoodCount;
+    [SerializeField] Transform exitMenu = null;
+    [SerializeField] int initWoodCount = 0;
+    [SerializeField] float enemyGenRate = 1f;
     #endregion
     public static GameCore I{get; private set;}
     [HideInInspector] public HeroState hero = null;
     [HideInInspector] public Transform SceneRoot => sceneRoot;
+    float enemyGenNum = 0;
     void Awake(){
       if(I is null){
         I = this;
       }
-      hero = heroTrans.GetComponent<HeroState>();
+      hero = pfHero.GetComponent<HeroState>();
     }
     void Start(){
-      
       for(int i=0; i<initWoodCount; i++){
-        float p = 1f + 3f*Random.Range(0, 1f);
-        float t = 360f*Random.Range(0,1f);
-        var pos = new Vector3(p*Mathf.Cos(t), p*Mathf.Sin(t), 0);
-        Transform wood = Instantiate(woodTrans, pos, Quaternion.identity);
+        var pos = RandomPlaceCircle(1f, 4f);
+        var wood = Instantiate(pfWood, pos, Quaternion.identity);
         wood.SetParent(sceneRoot);
       }
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+          exitMenu.gameObject.SetActive(true);
+          Time.timeScale = 0;
+        }
+        enemyGenNum += Time.deltaTime*enemyGenRate;
+        while(enemyGenNum>1f){
+          enemyGenNum -= 1f;
+          var pos = RandomPlaceCircle(1, 5f);
+          var enemy = Instantiate(pfEnemy, pos, Quaternion.identity);
+          enemy.SetParent(sceneRoot);
+        }
+    }
+    Vector3 RandomPlaceCircle(float innerRadius, float outterRadius){
+      float p = innerRadius + (outterRadius-innerRadius)*Random.Range(0, 1f);
+      float t = 360f*Random.Range(0,1f);
+      return new Vector3(p*Mathf.Cos(t), p*Mathf.Sin(t), 0);
+    }
+    public void OnExitOk(){
+      Application.Quit();
+    }
+    public void OnExitCancel(){
+      Time.timeScale = 1f;
+      exitMenu.gameObject.SetActive(false);
     }
   }
 }
