19f24be7de57186e1cd9f41fef10e876e3b200af
misdeer <719851601@qq.com>
2020-02-06 21:42:04 +0800

    adjust code

--- Development/Cold/Assets/Scripts/Trigger/HeatAura.cs
+++ Development/Cold/Assets/Scripts/Trigger/HeatAura.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 namespace Cold
 {
   public class HeatAura : MonoBehaviour{
+    FireHeapState fireHeap = null;
+    void Start(){
+      fireHeap = GetComponentInParent<FireHeapState>();
+    }
+    void Update(){
+      float rate = fireHeap.Health/Const.MaxHealth;
+      transform.localScale = Vector3.one * rate;
+      if(fireHeap.Health <= 0){
+        gameObject.SetActive(false);
+      }
+    }
     void OnTriggerStay2D(Collider2D other)
     {
       var pawn = other.transform.GetComponent<PawnState>();
